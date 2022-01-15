0c198f563df4e6397031d97c226204639588e138
j5pu <63794670+j5pu@users.noreply.github.com>
2022-01-15 20:06:19 +0100

    feat: first site

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from "next/link"
 import styles from '@styles/Home.module.css'
+import CodeBlock from "@components/codeblock"
 
 export default function Home() {
   return (
