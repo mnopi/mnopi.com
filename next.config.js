5e574ef545ccc745313b196638c6deb2790a7334
j5pu <63794670+j5pu@users.noreply.github.com>
2022-01-15 13:35:21 +0100

    feat: first site

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@
 module.exports = {
   reactStrictMode: true,
 
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config
+  },
+
   async redirects() {
     return [
       {
