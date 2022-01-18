7800cbd6248c729aff2e0aa7553f8aba47d7effc
j5pu <63794670+j5pu@users.noreply.github.com>
2022-01-18 15:32:35 +0100

    feat: first site

--- pages/api/git.js
+++ pages/api/git.js
@@ -3,23 +3,140 @@
 // GitHub REST API Reference: https://docs.github.com/en/rest/reference
 
 const { Octokit } = require("@octokit/core");
+const { shaurl } = require("shaurl");
 
-async function request(owner = process.env.VERCEL_GIT_REPO_OWNER, repo){
-  const octokit = new Octokit({ auth: process.env.TOKEN });
-  return await octokit.request("GET /repos/{owner}/{repo}", {
-    owner: owner,
-    repo: repo,
-  });
+function parameters(req, res, route= "") {
+  switch (req.query.index) {
+    case "repos":
+      req.query.key ??= "description";
+      req.query.repo ??= "actions";
+      return {
+        "route": `GET /${req.query.index}/{owner}/{repo}${route}`,
+        "options": {
+          owner: req.query.owner,
+          repo: req.query.repo
+        }};
+    case "search":
+      if (req.query.key == null) {
+        req.query.key ??= "repositories";
+        req.query.map ??= "name";
+      }
+      req.query.prefix = "homebrew-";
+      req.query.q ??= `${req.query.prefix}+user:${req.query.owner}`;
+      req.query.q = 'q=' + encodeURIComponent(req.query.q)
+      if (!req.query.q.includes(req.query.prefix)) {
+        req.query.prefix = ""
+      }
+      return {
+        "route": `GET /${req.query.index}/${req.query.key}${route}`,
+        "options": {
+          q: req.query.q
+        }};
+    case "users":
+      req.query.key ??= "id";
+      return {
+        "route": `GET /${req.query.index}/{username}${route}`,
+        "options": {
+          username: req.query.owner
+        }};
+  }
 }
 
-// curl -fsSL "https://mnopi.com/api/git?owner=j5pu&repo=actions&data=description"
-// curl -fsSL "https://mnopi.com/api/git?repo=actions&data=description"
-export default async function handler(req, res) {
-  await request(req.query.owner, req.query.repo)
-  .then(function (response) {
-      res.status(response.status).end(response.data[req.query.data])
+
+async function request(req, res, route= "", exit = true) {
+
+  const params = parameters(req, res, route);
+  if (req.query.log !== "0") {
+    console.log(req.query, params)
+  }
+
+  const octokit = new Octokit({ auth: process.env.TOKEN });
+  await octokit.request(params.route, params.options)
+  .then( function (response) {
+    if (req.query.log !== "0") {
+      console.log(response.data)
+    }
+    return response.data
   })
-  .catch(function (response) {
-    res.status(response.status).end()
+  .catch( function (response) {
+    if (exit) {
+      res.status(response.status).end()
+    }
+    return {}
   });
+
+}
+
+export default async function handler(req, res) {
+  if (
+    req.query.formula !== null ||
+    req.query.latest !== null ||
+    req.query.sha256 !== null ||
+    req.query.tag !== null
+  ) {
+    req.query.index ??= "repos";
+  }
+  req.query.formula ??= "0";
+  req.query.latest ??= "0";
+  req.query.sha256 ??= "0";
+  req.query.tag ??= "0";
+  req.query.index ??= "search";
+  req.query.owner ??= process.env.VERCEL_GIT_REPO_OWNER;
+  req.query.log ??= "0";
+
+  const data = await request(req, res);
+
+  switch (req.query.index) {
+    case "search":
+      const arr = data.map(x => x[req.query.map].replace(req.query.prefix, ""));
+      if (req.query.log !== "0") {
+        console.log(arr)
+      }
+      res.status(200).end(arr.join('\n'));
+      break;
+    default:
+      if (
+        req.query.formula === "0" &&
+        req.query.latest === "0" &&
+        req.query.sha256 === "0" &&
+        req.query.tag === "0"
+      ) {
+        res.status(200).end(data[req.query.key]);
+      } else {
+        const latest = await request(
+          req, res, "/releases/latest", false
+        )
+
+        let tag = ""
+        if (latest !== {}) {
+          tag = latest["tag_name"]
+        } else {
+          const tags = await request(req, res,"/tags")
+          tag = tags[0].name
+        }
+
+        const desc = data["description"]
+        const homepage = data["html_url"]
+        const url = `${data["html_url"]}/archive/${tag}.tar.gz`
+        const sha256 = shaurl(url)
+
+        if (req.query.log !== "0") {
+          console.log(desc)
+          console.log(homepage)
+          console.log(url)
+          console.log(sha256)
+          console.log(tag)
+        }
+
+        if (req.query.formula !== "0") {
+          res.status(200).end(`${desc}\n${homepage}\n${url}\n${sha256}\n`);
+        } else if (req.query.latest !== "0") {
+          res.status(200).end(url);
+        } else if (req.query.sha256 !== "0") {
+          res.status(200).end(sha256);
+        } else if (req.query.tag !== "0") {
+          res.status(200).end(sha256);
+        }
+      }
+  }
 }
