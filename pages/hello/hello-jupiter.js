a18670103140cb5916e0b7cec5b76c57d2971de7
José <63794670+j5pu@users.noreply.github.com>
2022-02-07 23:43:01 +0100

    Create hello-jupiter.js

---
+++ pages/hello/hello-jupiter.js
@@ -0,0 +1,9 @@
+module.exports = {
+    hello: function(subject) {
+      console.log('Jupiter says hello ' + subject);
+    },
+
+    bye: function(subject) {
+      console.log('Jupiter says goodbye ' + subject);
+    }
+};
