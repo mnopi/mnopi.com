cb0399b88fb8f2ef141c9354c58591a37189bc07
José <63794670+j5pu@users.noreply.github.com>
2022-02-07 14:26:40 +0100

    Update and rename [py].js to [name].js

--- pages/api/py/[name].js
+++ pages/api/py/[name].js
@@ -1,11 +1,14 @@
 // [py].js
 
+// curl -fsSL "https://mnopi.com/api/py" -> 404
+// curl -fsSL "https://mnopi.com/api/py/dev"
+
 export default function handler(request, response) {
   const dev = ["build", "darling", "ipython", "pip-tools", "pytest"];
   let body = dev.join('\n');
   let code = 200;
   
-  if (request.query !== "dev" && request.query !== null) {
+  if (request.query.name !== "dev" && request.query !== null) {
     body = request.query;
     code = 400;
   }
