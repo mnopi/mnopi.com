1ede7720651d6a334a30d9f5a1a92ebb7d450356
José <63794670+j5pu@users.noreply.github.com>
2022-02-08 00:14:42 +0100

    Update index.js

--- pages/py/dev/index.js
+++ pages/py/dev/index.js
@@ -1,2 +1,22 @@
-const dev = ["build", "darling", "ipython", "pip-tools", "pytest"];
-console.log(dev.join('\n'));
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <p>
+          build
+          darling
+          ipython
+          pip-tools
+          pytest
+        </p>
+      </main>
+    </div>
+  )
+}
