d08e0850f81006b1a9db088343cd12b216722936
José <63794670+j5pu@users.noreply.github.com>
2022-02-08 02:53:47 +0100

    Update index.js

--- pages/py/dev/index.js
+++ pages/py/dev/index.js
@@ -1,21 +1,10 @@
-import Head from 'next/head'
+//import Head from 'next/head'
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Dev dependencies</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
-        <p>
-          build<br>
-          darling<br>
-          ipython<br>
-          pip-tools<br>
-          pytest<br>
-        </p>
+        <p>build darling ipython pip-tools pytest</p>
       </main>
     </div>
   )
